a7508ac4efbc2add59fb8943ffe61cdad3c260b6
Alejandro <alexronda30@gmail.com>
2025-05-05 17:16:35 +0200

    added db initialisation
    added the initialisation of the database in the module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,34 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { User } from './user/user.entity';
+
+import { WebAuthnCredentials } from './web-authn-credentials/web-authn-credentials.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env'],}),
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (ConfigService: ConfigService) => ({
+          type: 'postgres',
+          host: ConfigService.get('DB_HOST'),
+          port: ConfigService.get<number>('DB_PORT'),
+          username: ConfigService.get('DB_USERNAME'),
+          password: ConfigService.get('DB_PASSWORD'),
+          database: ConfigService.get('DB_DATABASE'),
+          entities: [User, WebAuthnCredentials],
+          synchronize: ConfigService.get('NODE_ENV') !== 'production',
+          logging: ConfigService.get('NODE_ENV') !== 'development',
+        }),
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
